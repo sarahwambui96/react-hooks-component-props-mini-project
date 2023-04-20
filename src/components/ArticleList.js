83e0edaacb9ad4f355aa6b42be792676b3ee838e
sarahwambui96 <sarahwambui843@gmail.com>
2023-04-20 16:35:19 +0300

    completed assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Article from './Article'
+
+function ArticleList(props) {
+  const {posts} = props
+  return (
+    <main>
+        {posts.map(post =>(
+            <Article key={post.id} post={post} />
+        ))}
+    </main>
+  )
+}
+export default ArticleList
+
