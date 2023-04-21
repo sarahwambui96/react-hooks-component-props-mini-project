d0faedad59c2445a2f1b6a9c626f14527f6b36dd
sarahwambui96 <sarahwambui843@gmail.com>
2023-04-21 21:59:24 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,7 @@ console.log(blogData);
 function App() {
    return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      
       <Header name ={blogData.name}/>
       <About image={blogData.image} about={blogData.about}/>
       <ArticleList posts={blogData.posts}/>
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
+function makeEmojiList(minutes) {
+  const interval = minutes < 30 ? 5 : 10;
+  const emoji = minutes < 30 ? "☕️" : "🍱";
+
+  let emojis = "";
+  for (let i = 0; i < minutes; i += interval) {
+      emojis += emoji;
+  }
+
+  return emojis;
+}
  function Article(props) {
-  const {title, date="January 1, 1970", preview } = props.post
+  const {title, date="January 1, 1970", preview, minutes={minutes}} = props.post
+  const emojis = makeEmojiList(minutes);
+  
   return (
     <article>
         <h3>{title}</h3>
-        <small>{date}</small>
+        <small>{date}  {emojis} {minutes} min read</small>
         <p>{preview}</p>
     </article>
   )
